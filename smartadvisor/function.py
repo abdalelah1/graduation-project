3b6649e8643aa59ce9e86662ba8d3f45b22212f2
ataleb251 <69352738+ataleb251@users.noreply.github.com>
2023-10-23 20:54:15 +0300

    function split with correct logic

--- smartadvisor/function.py
+++ smartadvisor/function.py
@@ -1,9 +1,10 @@
 from .models import *
 from collections import Counter
+from django.core.exceptions import ObjectDoesNotExist
 
 def RemainingCourses(all_courses, completed_courses):
     all_courses_set = set(all_courses)
-    print("all_courses",all_courses)
+
     completed_courses_set = set(completed_courses)
     remaining_courses_set = all_courses_set - completed_courses_set
     remaining_courses = list(remaining_courses_set)
@@ -36,7 +37,7 @@ def letter_grade_to_numeric(grade):
     elif grade== 'F' :
         return 0
     else :
-        print("error on this grada ",grade)
+        
         return 0
 def calculate_gpa(grades):
     gpa_scale = {
@@ -55,10 +56,9 @@ def calculate_gpa(grades):
     total_credits = 0.0
 
     for grade, credits in grades:
-        print(grade , credits)
+
         gpa = gpa_scale.get(grade, 0) 
-        print(type(gpa)) 
-        print(type(credits)) 
+
         total_points += float(gpa) * float(credits)
         total_credits += float(credits)
     if total_credits == 0:
@@ -89,15 +89,15 @@ def get_students_details(student_id):
         return "Student not found"
     
     all_student_courses = Course_History.objects.filter(student=student)
-    print(all_student_courses)
+
     for course in all_student_courses:
         grade = letter_grade_to_numeric(course.degree)
         if course.course :
-            print('is it course ')
+          
             credits = course.course.credit  
             course_code = course.course.code
         else : 
-            print('is it univsersity course ')
+            
             credits = course.universit_course.credit  
             course_code = course.universit_course.code
         # uni_code = course.universit_course.code
@@ -134,4 +134,79 @@ def courses_with_remaining_students():
         courses_map[student_id] = remaining_courses_for_student
         
     return courses_map
+def count_students_per_course():
+    courses_map = courses_with_remaining_students()
+    course_counts = Counter()
 
+    # Count the number of students for each course
+    for student_id, course_code in courses_map.items():
+        for course in course_code:
+            course_counts[course] += 1
+
+    # Create a dictionary to store the result
+    course_data = {}
+
+    # Group the data by course ID
+    for student_id, course_code in courses_map.items():
+        for course in course_code:
+            if course in course_data:
+                course_data[course][1].append(student_id)
+                course_data[course][2] += 1
+            else:
+                course_data[course] = [course, [student_id], 1]
+
+    # Filter courses with 20 or more students
+    popular_courses = [data for data in course_data.values() if data[2] >= 20]
+    less_popular_courses = [data for data in course_data.values() if data[2] < 20]
+    test_courses = [data for data in course_data.values() ]
+
+    return popular_courses, less_popular_courses , test_courses
+def split_course_counts_by_conditions():
+    popular_courses, less_popular_courses , test_courses = count_students_per_course()
+  
+    # Define the conditions
+    course_College_required = []
+    course_College_not_required = []
+    course_major_required = []
+    course_major_not_required = []
+    course_universite_required = []
+    course_university_not_required = []    
+    course = None
+    for course_code, student_id, count in test_courses:
+        try: 
+            course = Course.objects.get(code=course_code)
+            print(course.name, course.code,course.is_reuqired , course.type.id )
+            course_type = course.type.id
+            is_required = course.is_reuqired  
+
+
+        except ObjectDoesNotExist:
+            try:
+                course = University_Courses.objects.get(code=course_code)
+                print('This is a university course:', course.name, course.code,course.is_reuqired , )
+
+                is_required = course.is_reuqired  
+                # هنا يمكنك إضافة المزيد من المعالجة إذا لزم الأمر
+            except ObjectDoesNotExist:
+                print('Course not found:', course_code)
+  
+        if  isinstance(course, Course):
+            if is_required and course_type==1:
+                course_major_required.append(((student_id, course_code, count)))
+            elif is_required and course_type==2:
+                 course_College_required.append(((student_id, course_code, count)))
+            elif is_required==False and course_type==1:
+                course_major_not_required.append(((student_id, course_code, count)))
+            elif is_required==False and course_type==2:
+                 course_College_not_required.append(((student_id, course_code, count)))
+        else:
+            if is_required :
+                course_universite_required.append((student_id, course_code, count))
+            else :
+                course_university_not_required.append((student_id, course_code, count))
+    print("course_major_required" , course_major_required)
+    print("course_major_not_required" , course_major_not_required)
+    print("course_College_required" , course_College_required)
+    print("course_College_not_required" , course_College_not_required)
+    print("course_universite_required" , course_universite_required)
+    print("course_university_not_required" , course_university_not_required)
